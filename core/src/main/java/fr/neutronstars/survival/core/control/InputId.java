eb2ee76b5d5cbabaac9fe8d994c4d0d7586264c7
Neutron-Pro <neutron@neutronstars.fr>
2026-03-09 12:45:54 +0100

    Add left click

--- core/src/main/java/fr/neutronstars/survival/core/control/InputId.java
+++ core/src/main/java/fr/neutronstars/survival/core/control/InputId.java
@@ -6,4 +6,5 @@ public class InputId {
     public static final byte LEFT = 2;
     public static final byte RIGHT = 3;
     public static final byte SPRINT = 4;
+    public static final byte MOUSE_LEFT_CLICK = 5;
 }
